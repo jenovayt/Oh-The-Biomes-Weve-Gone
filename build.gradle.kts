1f6fc8a8c7da60d44bbefee56490524bfc85e929
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-08-20 15:28:35 -0400

    Mark for Java 21 +

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,7 +113,7 @@ subprojects {
             setReleaseType(ReleaseType.BETA)
             setGameVersions(minecraftVersion)
             setCurseEnvironment(CurseEnvironment.BOTH)
-            setJavaVersions(JavaVersion.VERSION_17, JavaVersion.VERSION_18, JavaVersion.VERSION_19, JavaVersion.VERSION_20, JavaVersion.VERSION_21, JavaVersion.VERSION_22)
+            setJavaVersions(JavaVersion.VERSION_21, JavaVersion.VERSION_22)
             val softDepends = mutableListOf("wthit")
             curseDepends.optional.set(softDepends)
             modrinthDepends.optional.set(softDepends)
